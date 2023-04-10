5ba743b11f8015b6b1d74b1369aa0b14a50be677
Valdo <85319750+valdodotcom@users.noreply.github.com>
2023-04-10 18:13:52 +0000

    Dummy Login Page

--- src/pages/sign-in/Login.js
+++ src/pages/sign-in/Login.js
@@ -1,27 +1,28 @@
 import React, { useState } from 'react';
-import firebase from 'firebase/app/';
-import 'firebase/auth';
+// import firebase from 'firebase/app/';
+// import 'firebase/auth';
 
 export default function LoginPage () {
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
+//   const [email, setEmail] = useState('');
+//   const [password, setPassword] = useState('');
 
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    firebase.auth().signInWithEmailAndPassword(email, password)
-      .then((userCredential) => {
-        // User authenticated successfully
-      })
-      .catch((error) => {
-        // Error occurred while authenticating user
-      });
-  };
+//   const handleSubmit = (event) => {
+//     event.preventDefault();
+//     firebase.auth().signInWithEmailAndPassword(email, password)
+//       .then((userCredential) => {
+//         // User authenticated successfully
+//       })
+//       .catch((error) => {
+//         // Error occurred while authenticating user
+//       });
+//   };
 
   return (
-    <form onSubmit={handleSubmit}>
-      <input type="email" value={email} onChange={(e) => setEmail(e.target.value)} />
-      <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} />
-      <button type="submit">Log in</button>
-    </form>
+    <h2>Login Page</h2>
+    // <form onSubmit={handleSubmit}>
+    //   <input type="email" value={email} onChange={(e) => setEmail(e.target.value)} />
+    //   <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} />
+    //   <button type="submit">Log in</button>
+    // </form>
   );
 };
