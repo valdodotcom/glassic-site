62ee7f7c578f543661c4a4eae4ce6fbd8d58f83b
Valdo <85319750+valdodotcom@users.noreply.github.com>
2023-04-10 18:05:08 +0000

    Firebase Added to App

--- src/index.js
+++ src/index.js
@@ -4,6 +4,29 @@ import './index.css';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
 import { CartContextProvider } from './pages/store/cart-context';
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDT73fQFTFvAYT_LK55fYLsUQpL60uf0eI",
+  authDomain: "glassic-site.firebaseapp.com",
+  databaseURL: "https://glassic-site-default-rtdb.firebaseio.com",
+  projectId: "glassic-site",
+  storageBucket: "glassic-site.appspot.com",
+  messagingSenderId: "174506103865",
+  appId: "1:174506103865:web:65d8cad6d71f3896abe9e8",
+  measurementId: "G-1RFW2J0SKE"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
