3bbc9c9cc3d9487d0fd70c24f3eb2fb713c8b275
Valdo <85319750+valdodotcom@users.noreply.github.com>
2023-04-10 18:35:27 +0000

    Cleanup for Github Actions

--- src/index.js
+++ src/index.js
@@ -5,8 +5,8 @@ import App from './App';
 import { BrowserRouter } from 'react-router-dom';
 import { CartContextProvider } from './pages/store/cart-context';
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
+// import { initializeApp } from "firebase/app";
+// import { getAnalytics } from "firebase/analytics";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -24,7 +24,7 @@ const firebaseConfig = {
 };
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
+// const app = initializeApp(firebaseConfig);
 // const analytics = getAnalytics(app);
 
 
