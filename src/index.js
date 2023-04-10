6731c8b9774f87847375a4c3d1fce0cd2856ad96
Valdo <85319750+valdodotcom@users.noreply.github.com>
2023-04-10 18:37:15 +0000

    Cleanup for Github Actions

--- src/index.js
+++ src/index.js
@@ -12,16 +12,16 @@ import { CartContextProvider } from './pages/store/cart-context';
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: "AIzaSyDT73fQFTFvAYT_LK55fYLsUQpL60uf0eI",
-  authDomain: "glassic-site.firebaseapp.com",
-  databaseURL: "https://glassic-site-default-rtdb.firebaseio.com",
-  projectId: "glassic-site",
-  storageBucket: "glassic-site.appspot.com",
-  messagingSenderId: "174506103865",
-  appId: "1:174506103865:web:65d8cad6d71f3896abe9e8",
-  measurementId: "G-1RFW2J0SKE"
-};
+// const firebaseConfig = {
+//   apiKey: "AIzaSyDT73fQFTFvAYT_LK55fYLsUQpL60uf0eI",
+//   authDomain: "glassic-site.firebaseapp.com",
+//   databaseURL: "https://glassic-site-default-rtdb.firebaseio.com",
+//   projectId: "glassic-site",
+//   storageBucket: "glassic-site.appspot.com",
+//   messagingSenderId: "174506103865",
+//   appId: "1:174506103865:web:65d8cad6d71f3896abe9e8",
+//   measurementId: "G-1RFW2J0SKE"
+// };
 
 // Initialize Firebase
 // const app = initializeApp(firebaseConfig);
