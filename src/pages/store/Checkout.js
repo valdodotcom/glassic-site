b183822711ca3ade4bd49efb26fa1934ae7694c4
Valdo <85319750+valdodotcom@users.noreply.github.com>
2023-04-15 12:46:48 +0000

    Revert "Isolating Checkout Problem"
    This reverts commit 72ebaf734eaf3543605fefbc34f94438ea3c205f.

--- src/pages/store/Checkout.js
+++ src/pages/store/Checkout.js
@@ -1,50 +1,51 @@
-// import { useContext, useState } from 'react';
-// import CartContext from './cart-context';
-// import { useNavigate } from 'react-router';
+import { useContext, useState } from 'react';
+import CartContext from './cart-context';
+import styles from './Checkout.module.css';
+import { useNavigate } from 'react-router';
 
 export default function CheckoutPage() {
-  // const cartCtx = useContext(CartContext);
-  // const [billingInfo, setBillingInfo] = useState({});
-  // const navigate = useNavigate();
+  const cartCtx = useContext(CartContext);
+  const [billingInfo, setBillingInfo] = useState({});
+  const navigate = useNavigate();
 
-  // const handleCheckout = () => {
-  //   const orderData = {
-  //     totalPrice: cartCtx.totalPrice.toFixed(2),
-  //     items: cartCtx.cartItems,
-  //     billing: billingInfo // add billingInfo to orderData
-  //   };
+  const handleCheckout = () => {
+    const orderData = {
+      totalPrice: cartCtx.totalPrice.toFixed(2),
+      items: cartCtx.cartItems,
+      billing: billingInfo // add billingInfo to orderData
+    };
 
-  //   fetch('https://glassic-site-default-rtdb.firebaseio.com/order.json', {
-  //     method: 'POST',
-  //     body: JSON.stringify(orderData),
-  //     headers: {
-  //       'Content-Type': 'application/json'
-  //     }
-  //   })
-  //     .then(response => {
-  //       if (!response.ok) {
-  //         throw new Error('Could not add order');
-  //       }
-  //       cartCtx.clearCart();
-  //       navigate('/');
-  //     })
-  //     .catch(error => {
-  //       console.error(error);
-  //     });
-  // };
+    fetch('https://glassic-site-default-rtdb.firebaseio.com/order.json', {
+      method: 'POST',
+      body: JSON.stringify(orderData),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    })
+      .then(response => {
+        if (!response.ok) {
+          throw new Error('Could not add order');
+        }
+        cartCtx.clearCart();
+        navigate('/');
+      })
+      .catch(error => {
+        console.error(error);
+      });
+  };
 
-  // const handleBillingInfoChange = (event) => {
-  //   setBillingInfo({
-  //     ...billingInfo,
-  //     [event.target.name]: event.target.value
-  //   });
-  // };
+  const handleBillingInfoChange = (event) => {
+    setBillingInfo({
+      ...billingInfo,
+      [event.target.name]: event.target.value
+    });
+  };
 
   return (
-    <section>
+    <section className={styles.checkoutSection}>
       <h2>Checkout</h2>
 
-        {/* <h3>Billing Information</h3>
+        <h3>Billing Information</h3>
         <label htmlFor="firstName">First Name:</label>
         <input type="text" id="firstName" name="firstName" onChange={handleBillingInfoChange} required />
         <label htmlFor="lastName">Last Name:</label>
@@ -64,7 +65,7 @@ export default function CheckoutPage() {
         <label htmlFor="mobileMoneyNumber">Mobile Money Number:</label>
         <input type="text" id="mobileMoneyNumber" name="mobileMoneyNumber" required />
 
-        <button onClick={handleCheckout}>Place Order</button> */}
+        <button onClick={handleCheckout}>Place Order</button>
     </section>
   );
 }
